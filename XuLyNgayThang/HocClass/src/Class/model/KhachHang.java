0c143c21d0554684ae4174084edc1f292fb334a6
JaVa <minhnguyen00827@gmail.com>
2018-12-25 10:09:37 +0700

    manh

--- XuLyNgayThang/HocClass/src/Class/model/KhachHang.java
+++ XuLyNgayThang/HocClass/src/Class/model/KhachHang.java
@@ -10,9 +10,56 @@ package Class.model;
  * @author nguye
  */
 public class KhachHang {
+    private String ma;
+    private String ten;
+    private int tuoi;
+    private String diaChi;
 
-    public KhachHang() {
-        System.out.println(" contructor mặc định tự động được gọi");
+    public KhachHang(String ma, String ten, int tuoi, String diaChi) {
+        this.ma = ma;
+        this.ten = ten;
+        this.tuoi = tuoi;
+        this.diaChi = diaChi;
     }
+    
+    
+    public KhachHang() {
+        System.out.println(" Contructor mặc định tự động được gọi");
+    }
+
+    public String getMa() {
+        return ma;
+    }
+
+    public void setMa(String ma) {
+        this.ma = ma;
+    }
+
+    public String getTen() {
+        return ten;
+    }
+
+    public void setTen(String ten) {
+        this.ten = ten;
+    }
+
+    public int getTuoi() {
+        return tuoi;
+    }
+
+    public void setTuoi(int tuoi) {
+        this.tuoi = tuoi;
+    }
+
+    public String getDiaChi() {
+        return diaChi;
+    }
+
+    public void setDiaChi(String diaChi) {
+        this.diaChi = diaChi;
+    }
+    
+    
+    
   
 }
