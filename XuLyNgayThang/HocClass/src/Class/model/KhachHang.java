d7df18807dc18ad7938a65a55404df04678c0de3
JaVa <minhnguyen00827@gmail.com>
2018-12-25 10:00:37 +0700

    manh

---
+++ XuLyNgayThang/HocClass/src/Class/model/KhachHang.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Class.model;
+
+/**
+ *
+ * @author nguye
+ */
+public class KhachHang {
+
+    public KhachHang() {
+        System.out.println(" contructor mặc định tự động được gọi");
+    }
+  
+}
---
+++ XuLyNgayThang/HocClass/src/Class/test/Main.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Class.test;
+
+import Class.model.KhachHang;
+
+/**
+ *
+ * @author nguye
+ */
+public class Main {
+    public static void main(String[] args){
+        KhachHang Teo = new KhachHang();
+    }
+    
+}
