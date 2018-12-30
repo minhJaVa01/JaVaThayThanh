37619ab14e6be85be7191bc9b497db68cc7cdc35
JaVa <minhnguyen00827@gmail.com>
2018-12-30 23:30:57 +0700

    manh

---
+++ LitsView/app/src/main/java/demo/quoctuan/com/litsview/MainActivity.java
@@ -0,0 +1,13 @@
+package demo.quoctuan.com.litsview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
