ceac2cf38c56961dcb684fa9a86aa000c0072858
JaVa <minhnguyen00827@gmail.com>
2019-01-01 12:14:02 +0700

    manh

--- ListView_NangCao/app/src/main/java/demo/quoctuan/com/adapter/DanhBaAdapter.java
+++ ListView_NangCao/app/src/main/java/demo/quoctuan/com/adapter/DanhBaAdapter.java
@@ -3,10 +3,6 @@ package demo.quoctuan.com.adapter;
 import android.app.Activity;
 import android.content.Context;
 import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 
 import java.util.List;
@@ -29,11 +25,5 @@ public class DanhBaAdapter extends ArrayAdapter<DanhBa> {
 
 
     }
-
-
-    public View getView(int position,  View convertView,  ViewGroup parent) {
-        LayoutInflater intInflater =this.context.getLayoutInflater();
-        View row = intInflater.inflate(this.resource,null);
-        return super.getView(position, convertView, parent);
-    }
+    
 }
