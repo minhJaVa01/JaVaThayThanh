0e420292fd7bed6f9cf1d748b4106ab7f8e41057
JaVa <minhnguyen00827@gmail.com>
2019-01-06 11:24:39 +0700

    manh

--- HocDatePickerandTimePicker2/app/src/main/java/vn/edu/topica/hocdatepickerandtimepicker2/MainActivity.java
+++ HocDatePickerandTimePicker2/app/src/main/java/vn/edu/topica/hocdatepickerandtimepicker2/MainActivity.java
@@ -1,11 +1,10 @@
-package vn.edu.topica.datepickerandtimepicker;
+package vn.edu.topica.hocdatepickerandtimepicker2;
 
 import android.app.DatePickerDialog;
 import android.app.TimePickerDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.ImageButton;
 import android.widget.TextView;
@@ -14,16 +13,12 @@ import android.widget.TimePicker;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 
-import static java.util.Calendar.YEAR;
-
 public class MainActivity extends AppCompatActivity {
-
-    TextView txtDate,txtTime;
     ImageButton btnDate,btnTime;
-    Calendar cal;
-    SimpleDateFormat sDF1 = new SimpleDateFormat("dd/MM/yyyy");
-    SimpleDateFormat sDF2 = new SimpleDateFormat("HH:mm");
-
+    TextView txtDate,txtTime;
+    Calendar calendar = Calendar.getInstance();
+    SimpleDateFormat sDF1= new SimpleDateFormat("dd/MM/yyyy");
+    SimpleDateFormat sDf2 = new SimpleDateFormat("HH:mm:ss");
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -36,66 +31,65 @@ public class MainActivity extends AppCompatActivity {
         txtDate = this.<TextView>findViewById(R.id.txtDate);
         txtTime = this.<TextView>findViewById(R.id.txtTime);
         btnDate = this.<ImageButton>findViewById(R.id.btnDate);
-        btnTime = this.<ImageButton>findViewById(R.id.btnTime);
-        cal =Calendar.getInstance();
-        txtDate.setText(sDF1.format(cal.getTime()));
-        txtTime.setText((sDF2.format(cal.getTime())));
+        btnTime= this.<ImageButton>findViewById(R.id.btnTime);
+        calendar =Calendar.getInstance();
+        txtDate.setText(sDF1.format(calendar.getTime()));
+        txtTime.setText(sDf2.format(calendar.getTime()));
     }
 
     private void addEvents() {
-
-        //Xử Lý DATE
         btnDate.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                xuLyDate();
+                xuLyDatepicker();
             }
         });
-        //
         btnTime.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                xuLyTimepiker();
+                xuLyTimePicKer();
             }
         });
 
     }
 
-    private void xuLyTimepiker() {
+    private void xuLyTimePicKer() {
         TimePickerDialog.OnTimeSetListener callBack = new TimePickerDialog.OnTimeSetListener() {
             @Override
             public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
-                cal.set(Calendar.HOUR_OF_DAY,hourOfDay);
-                cal.set(Calendar.MINUTE,minute);
-                txtTime.setText(sDF2.format(cal.getTime()));
+                calendar.set(Calendar.HOUR_OF_DAY,hourOfDay);
+                calendar.set(Calendar.MINUTE,minute);
+                txtTime.setText(sDf2.format(calendar.getTime()));
             }
         };
         TimePickerDialog timePickerDialog = new TimePickerDialog(
                 MainActivity.this,
                 callBack,
-                cal.get(Calendar.HOUR_OF_DAY),
-                cal.get(Calendar.MINUTE),
+                calendar.get(Calendar.HOUR_OF_DAY),
+                calendar.get(Calendar.MINUTE),
                 true
-        );
+                );
         timePickerDialog.show();
     }
 
-    private void xuLyDate() {
+    private void xuLyDatepicker() {
         DatePickerDialog.OnDateSetListener callBack = new DatePickerDialog.OnDateSetListener() {
             @Override
             public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
-                cal.set(Calendar.YEAR,year);
-                cal.set(Calendar.MONTH,month);
-                cal.set(Calendar.DAY_OF_MONTH,dayOfMonth);
-                txtDate.setText(sDF1.format(cal.getTime()));
+                calendar.set(Calendar.YEAR,year);
+                calendar.set(Calendar.MONTH,month);
+                calendar.set(Calendar.DAY_OF_MONTH,dayOfMonth);
+                txtDate.setText(sDF1.format(calendar.getTime()));
             }
         };
-        DatePickerDialog datePickerDialog = new DatePickerDialog(
+        DatePickerDialog dialog = new DatePickerDialog(
                 MainActivity.this,
                 callBack,
-                cal.get(Calendar.YEAR),
-                cal.get(Calendar.MONTH),
-                cal.get(Calendar.DAY_OF_MONTH));
-        datePickerDialog.show();
+                calendar.get(Calendar.YEAR),
+                calendar.get(Calendar.MONTH),
+                calendar.get(Calendar.DAY_OF_MONTH)
+                );
+        dialog.show();
+
     }
 }
